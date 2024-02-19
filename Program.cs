4af23abdece8b9d727d460e1873396f590a392d4
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-19 23:53:18 +0330

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace ExcelFileCategorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
