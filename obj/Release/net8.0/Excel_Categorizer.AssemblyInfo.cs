380aced1a08d9fb15003a77d1546611d70b7ea4c
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-21 12:44:55 +0330

    chore: dotnet build

--- obj/Release/net8.0/Excel_Categorizer.AssemblyInfo.cs
+++ obj/Release/net8.0/Excel_Categorizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excel_Categorizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f938ce548855b94d598c887b3349c8ead9c323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88108b3bc8abe3c67cfc9e017444fe499fe006f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excel_Categorizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excel_Categorizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
