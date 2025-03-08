3bc63f0a8638ace7deac6be0e74a84a635089224
Rezishon.Pc <rezashahmoradi.81@outlook.com>
2025-03-08 14:29:05 +0330

    feat: add setting region
    The setting region will be use for users to set thier own path and other
    setting like the specific column names to be handled by app.

--- Excel_To_Access/Program.cs
+++ Excel_To_Access/Program.cs
@@ -7,5 +7,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        #region Setting
+
+        string mainFilePath = @"/home/rezishon/storage/Project/Excel_Apps/Excel_To_Access/Data/14031217.xlsx";
+        string resultFilePath = @"/home/rezishon/storage/Project/Excel_Apps/Excel_To_Access/Data/result.xlsx";
+        string resultFileSheetName = "NewSheet";
+        Dictionary<string, string> knownColumn = new Dictionary<string, string>()
+        {
+            { "ساعت ثبت بارنامه", "registeringtime" },
+            { "کارت هوشمند وسیله", "trucksmartcardno" },
+            {"تاریخ صدور", "registeringdate" },
+        };
+
+        #endregion
     }
 }
